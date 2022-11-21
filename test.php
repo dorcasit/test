88e254b3b8919cd437cb3d5c09b2ef5547f38f42
Dorcas IT <100519509+dorcasit@users.noreply.github.com>
2022-11-21 15:48:45 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,24 @@
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://google.com
+ * @since             1.0.0
+ * @package           Test
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Test
+ * Plugin URI:        https://google.com
+ * Description:       Test plugin
+ * Version:           1.0.0
+ * Author:            Test
+ * Author URI:        https://google.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       test
+ * Domain Path:       /languages
+ */
